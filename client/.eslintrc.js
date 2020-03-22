00a1b329317ad9a18dfd3cbece25fc4fc14748c9
Ali <ali@opnetwork.gg>
2020-03-22 22:50:40 +0300

    Moved to NPM and updates

---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ]
+};
